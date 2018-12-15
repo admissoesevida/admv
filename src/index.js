047da6ba60694a5c873c7266e31bc193dc966ce8
Paulo Cézar Júnior <paulocezar.francisco.junior@gmail.com>
2018-12-15 13:36:59 -0200

    Revamp

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import app from './app';
-
-const { PORT = 8080, MODE = 'PROD' } = process.env;
-app.listen(PORT, () => 
-    console.log(`Listening on port ${PORT} (${MODE} mode)`)); // eslint-disable-line no-console
