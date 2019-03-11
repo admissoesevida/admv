457d67cc057ec3c11f44de63dd560fe3d6869ff7
paulojunior95 <paulocezar.francisco.junior@gmail.com>
2019-03-10 23:29:37 -0300

    Alterada porta do API para 5000 (#3)

--- api/src/server.js
+++ api/src/server.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import db from "./models";
 import handleMembers from "./controllers/member";
 
+const PORT = 5000;
 const env = process.env.NODE_ENV || "development";
 const app = express();
 
@@ -16,7 +17,7 @@ app.get("/", (req, res) => {
     .send({ environment: env, description: "ADMV Rest API" });
 });
 
-app.listen(3000);
-console.log("Listening port ", 3000); // eslint-disable-line no-console
+app.listen(PORT);
+console.log("Listening port ", PORT); // eslint-disable-line no-console
 
 export default app;
