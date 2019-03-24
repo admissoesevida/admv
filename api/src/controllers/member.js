5854062d821fe57788d8f6550874989e3c544806
paulojunior95 <paulocezar.francisco.junior@gmail.com>
2019-03-24 11:39:36 -0300

    Source format

--- api/src/controllers/member.js
+++ api/src/controllers/member.js
@@ -1,13 +1,13 @@
 export default (app, db) => {
-  app.get("/members", (req, res) =>
+  app.get('/members', (req, res) =>
     db.member.findAll().then(result => res.json(result))
   );
 
-  app.get("/member/:id", (req, res) =>
-    db.member.findByPk(req.params.id).then(result => res.json(result)) //findById is deprecated
+  app.get('/member/:id', (req, res) =>
+    db.member.findByPk(req.params.id).then(result => res.json(result))
   );
 
-  app.post("/member", (req, res) =>
+  app.post('/member', (req, res) =>
     db.member
       .create({
         name: req.body.name,
@@ -16,20 +16,17 @@ export default (app, db) => {
       .then(result => res.json(result))
   );
 
-  app.put("/member/:id", (req, res) =>
+  app.put('/member/:id', (req, res) =>
     db.member
-      .update(
-        req.body,
-        {
-          where: {
-            id: req.params.id
-          }
+      .update(req.body, {
+        where: {
+          id: req.params.id
         }
-      )
+      })
       .then(result => res.json(result))
   );
 
-  app.delete("/member/:id", (req, res) =>
+  app.delete('/member/:id', (req, res) =>
     db.member
       .destroy({
         where: {
--- api/src/server.js
+++ api/src/server.js
@@ -1,11 +1,11 @@
-import express from "express";
-import bodyParser from "body-parser";
-import db from "./models";
-import handleMembers from "./controllers/member";
-import handleProviders from "./controllers/provider";
+import bodyParser from 'body-parser';
+import db from './models';
+import express from 'express';
+import handleMembers from './controllers/member';
+import handleProviders from './controllers/provider';
 
 const PORT = 5000;
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || 'development';
 const app = express();
 
 app.use(bodyParser.json());
@@ -13,15 +13,15 @@ app.use(bodyParser.json());
 handleMembers(app, db);
 handleProviders(app, db);
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   return res
     .status(200)
-    .send({ environment: env, description: "ADMV Rest API" });
+    .send({ environment: env, description: 'ADMV Rest API' });
 });
 
-if(!module.parent){
+if (!module.parent) {
   app.listen(PORT);
-  console.log("Listening port ", PORT); // eslint-disable-line no-console
+  console.log('Listening port ', PORT); // eslint-disable-line no-console
 }
 
 export default app;
