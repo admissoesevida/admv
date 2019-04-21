74e03e8d5f8a548fa95b6406ff5e1e52262e255b
paulojunior95 <paulocezar.francisco.junior@gmail.com>
2019-04-21 00:37:20 -0300

    Atualizados scripts de start e destroy e dockerfiles (#66)

--- destroy.sh
+++ destroy.sh
@@ -2,8 +2,12 @@
 
 SERVICE_ID=${1-api}
 
-CONTAINER_ID=$(docker ps | grep admv-$SERVICE_ID | awk '{print $1}')
+CONTAINER_ID=$(docker ps -aqf name=admv-$SERVICE_ID)
 
-docker stop $CONTAINER_ID
-
-docker rm $CONTAINER_ID
+if [ ! -z "$CONTAINER_ID" ]; then
+  echo "Stopping and removing container $CONTAINER_ID from service admv-$SERVICE_ID"
+  docker stop $CONTAINER_ID > /dev/null
+  docker rm $CONTAINER_ID > /dev/null
+else
+  echo "There is no container for this service: admv-$SERVICE_ID"
+fi
