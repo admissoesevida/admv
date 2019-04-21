8ad144e6a53505c85324aaeb2d5a0a5fb1afce45
Paulo Cézar Júnior <paulocezar.francisco.junior@gmail.com>
2019-04-21 12:57:03 -0300

    Atualizados scripts de start e destroy e dockerfiles (#66) (#67)

--- start.sh
+++ start.sh
@@ -2,8 +2,28 @@
 
 SERVICE_ID=${1-api}
 SERVICE_CONTAINER_PORT=${2-5000}
-SERVICE_HOST_PORT=${3-5000}
+SERVICE_HOST_PORT=${3-$SERVICE_CONTAINER_PORT}
 
-docker build . admv-$SERVICE_ID
+echo "Going to run $SERVICE_ID on port $SERVICE_HOST_PORT (internal $SERVICE_CONTAINER_PORT)"
 
-docker run -td -p$SERVICE_CONTAINER_PORT:$SERVICE_HOST_PORT --name admv-$SERVICE_ID admv-$SERVICE_ID
+echo "Copying config files to temp folder..."
+mkdir ./$SERVICE_ID/tmp
+cp /keybase/team/admv/$SERVICE_ID/.env ./$SERVICE_ID/tmp
+
+echo "Building docker image..."
+docker build ./$SERVICE_ID -t admv-$SERVICE_ID > /dev/null
+
+CONTAINER_ID=$(docker ps -aqf name=admv-$SERVICE_ID)
+
+if [ ! -z "$CONTAINER_ID" ]; then
+  echo "Removing old container..."
+  docker rm -f $CONTAINER_ID
+fi
+
+echo "Running $SERVICE_ID on port $SERVICE_HOST_PORT"
+docker run -td -p$SERVICE_CONTAINER_PORT:$SERVICE_HOST_PORT --name admv-$SERVICE_ID admv-$SERVICE_ID > /dev/null
+
+echo "Cleaning up temp files..."
+rm -rf ./$SERVICE_ID/tmp
+
+echo "Done"
