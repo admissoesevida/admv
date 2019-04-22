7ff8ce68a485f8a0a75660a660d3808e2d0b3cbb
paulojunior95 <paulocezar.francisco.junior@gmail.com>
2019-04-21 21:44:38 -0300

    Criada tela de visualização de Expenses (closes #57)

--- app/src/pages/Expenses.tsx
+++ app/src/pages/Expenses.tsx
@@ -1,12 +1,38 @@
 import * as React from "react";
+import Title from "../components/Title";
+import TableView from "../components/TableView";
+
+export interface Expense {
+  id: number;
+  date: string;
+  value: number;
+  note: string;
+  expenseTypeId: number;
+  providerId: number;
+}
 
 export default class Expenses extends React.Component {
   public static displayName: string = "Expenses";
 
+  public state = {
+    list: []
+  };
+
+  public componentDidMount(): void {
+    fetch("/api/expenses")
+      .then((res): Promise<Expense[]> => res.json())
+      .then((list): void => this.setState({ list }));
+  }
+
   public render(): React.ReactNode {
     return (
       <div className="page-expenses">
-        <p>Expenses</p>
+        <Title text="Expenses" />
+        <TableView
+          list={this.state.list}
+          headers={["ID", "Data", "ID de Tipo", "ID de Fornecedor", "Valor"]}
+          fields={["id", "date", "expenseTypeId", "providerId", "value"]}
+        />
       </div>
     );
   }
