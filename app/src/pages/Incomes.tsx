e195c1b33d4028f86049434b99e3500a64b0e700
paulojunior95 <paulocezar.francisco.junior@gmail.com>
2019-04-21 21:44:11 -0300

    Criado tela de Visualização de Incomes (closes #56)

--- app/src/pages/Incomes.tsx
+++ app/src/pages/Incomes.tsx
@@ -1,12 +1,38 @@
 import * as React from "react";
+import Title from "../components/Title";
+import TableView from "../components/TableView";
+
+export interface Income {
+  id: number;
+  date: string;
+  value: number;
+  note: string;
+  incomeTypeId: number;
+  memberId: number;
+}
 
 export default class Incomes extends React.Component {
   public static displayName: string = "Incomes";
 
+  public state = {
+    list: []
+  };
+
+  public componentDidMount(): void {
+    fetch("/api/incomes")
+      .then((res): Promise<Income[]> => res.json())
+      .then((list): void => this.setState({ list }));
+  }
+
   public render(): React.ReactNode {
     return (
       <div className="page-incomes">
-        <p>Incomes</p>
+        <Title text="Incomes" />
+        <TableView
+          list={this.state.list}
+          headers={["ID", "Data", "ID de Tipo", "ID de Membro", "Valor"]}
+          fields={["id", "date", "incomeTypeId", "memberId", "value"]}
+        />
       </div>
     );
   }
